fe6920897d5feb81d92a15b31194392ff268fd21
Fabiola <fabi.ampuero.chalco@gmail.com>
2016-12-20 08:20:20 -0500

    ejercicio 2 terminado

---
+++ main.js
@@ -0,0 +1,11 @@
+var num1, num2, num3, suma, resultado;
+
+num1 = prompt("Por favor, ingresa un número");
+num2 = prompt("Por favor, ingresa otro número");
+num3 = prompt("Por favor, ingresa otro número");
+
+suma = parseFloat(num1)+parseFloat(num2)+parseFloat(num3);
+
+resultado = suma/3;
+
+alert(resultado);
